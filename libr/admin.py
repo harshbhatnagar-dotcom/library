820bea63d3a5e75b6f453e777f555977311c315c
harshbhatnagar-dotcom <harsh210806@gmail.com>
2025-07-02 23:08:03 +0530

    Update admin.py

--- libr/admin.py
+++ libr/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from . models import homeimage,Book,Student,Teacher,IssuedBook
+from . models import homeimage,Book,Student,Teacher,IssuedBook,EJournal‎
 
 admin.site.register(homeimage)
 admin.site.register(Book)
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(IssuedBook)
+admin.site.register(EJournal‎)
